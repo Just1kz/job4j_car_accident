e755059817de578cf2c4d66010a210c2c0034452
Just1lz <exelent.pride@rambler.ru>
2021-04-09 00:03:18 +0700

    Initial commit

---
+++ src/main/java/ru/job4j/car/accident/SomeLogic.java
@@ -0,0 +1,7 @@
+package ru.job4j.car.accident;
+
+public class SomeLogic {
+    public static boolean test() {
+        return true;
+    }
+}
---
+++ src/test/java/ru/job4j/car/accident/SomeLogicTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.car.accident;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SomeLogicTest {
+
+    @Test
+    public void test1() {
+        assertTrue(SomeLogic.test());
+    }
+}
