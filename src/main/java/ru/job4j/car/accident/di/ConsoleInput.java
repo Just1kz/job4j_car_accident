ae63e31f920c8d47445b5c0574b15c117f70c7c5
Just1lz <exelent.pride@rambler.ru>
2021-04-09 01:18:06 +0700

    2. Сканиварование проекта [#409533]

--- src/main/java/ru/job4j/car/accident/di/ConsoleInput.java
+++ src/main/java/ru/job4j/car/accident/di/ConsoleInput.java
@@ -1,5 +1,8 @@
 package ru.job4j.car.accident.di;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ConsoleInput {
     public void print(String string) {
         System.out.println(string);
