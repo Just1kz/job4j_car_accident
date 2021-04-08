0f0d3d8e3681d92296d732a1ab9b906a2ef2fddd
Just1lz <exelent.pride@rambler.ru>
2021-04-09 01:30:09 +0700

    4. Время жизни (Scope) [#409532]

--- src/main/java/ru/job4j/car/accident/di/ConsoleInput.java
+++ src/main/java/ru/job4j/car/accident/di/ConsoleInput.java
@@ -1,8 +1,10 @@
 package ru.job4j.car.accident.di;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class ConsoleInput {
     public void print(String string) {
         System.out.println(string);
