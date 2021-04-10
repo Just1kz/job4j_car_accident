b797ed17889020f8a3cdcfc96c60e7c410f05f51
Just1lz <exelent.pride@rambler.ru>
2021-04-11 00:33:34 +0700

    2. IndexControl. Таблица и вид. [#409528]

--- src/main/java/ru/job4j/car/accident/repository/AccidentMem.java
+++ src/main/java/ru/job4j/car/accident/repository/AccidentMem.java
@@ -4,17 +4,24 @@ import org.springframework.stereotype.Repository;
 import ru.job4j.car.accident.model.Accident;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class AccidentMem {
-    private final Map<Integer, Accident> accidentData = new HashMap<>();
+    private final Map<Integer, Accident> accidentData = new ConcurrentHashMap<>();
     private Integer count = 0;
 
-    public Map<Integer, Accident> getAccidentData() {
-        return accidentData;
+    public static AccidentMem of() {
+        AccidentMem accidentMem = new AccidentMem();
+        accidentMem.addAccident(new Accident("Проехал на красный",
+                "водитель машины под номером таким то.....", "Красный проспект 45"));
+        accidentMem.addAccident(new Accident("Пересёк двойную сплошную",
+                "водитель машины под номером таким то.....", "Красный проспект 15"));
+        accidentMem.addAccident(new Accident("Разворот в неположенном месте",
+                "водитель машины под номером таким то.....", "Красный проспект 35"));
+        return accidentMem;
     }
 
     public void addAccident(Accident accident) {
