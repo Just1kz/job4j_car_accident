5d719748d18050597045cc6aef367e853380fdc4
Just1lz <exelent.pride@rambler.ru>
2021-04-11 21:55:32 +0700

    0. Spring DataSource [#409547]

--- src/main/java/ru/job4j/car/accident/controller/AccidentControl.java
+++ src/main/java/ru/job4j/car/accident/controller/AccidentControl.java
@@ -6,34 +6,34 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.job4j.car.accident.model.Accident;
-import ru.job4j.car.accident.repository.AccidentMem;
+import ru.job4j.car.accident.repository.AccidentJdbcTemplate;
 
 @Controller
 public class AccidentControl {
-    private final AccidentMem accidents = AccidentMem.of();
+    private final AccidentJdbcTemplate accidents;
 
-//    public AccidentControl(AccidentMem accidents) {
-//        this.accidents = accidents;
-//    }
+    public AccidentControl(AccidentJdbcTemplate accidents) {
+        this.accidents = accidents;
+    }
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("allAccident", accidents.showAllAccident());
+        model.addAttribute("allAccident", accidents.getAllAccidents());
         return "index";
     }
 
     @GetMapping("/create")
     public String create(Model model) {
-        model.addAttribute("types", accidents.showAllAccidentType());
-        model.addAttribute("rules", accidents.showAllRule());
+        model.addAttribute("types", accidents.getAllAccidentType());
+        model.addAttribute("rules", accidents.getAllRules());
         return "accident/create";
     }
 
     @GetMapping("/edit")
     public String edit(@RequestParam("id") int id, Model model) {
-        model.addAttribute("accident", accidents.findByIdAccident(id));
-        model.addAttribute("types", accidents.showAllAccidentType());
-        model.addAttribute("rules", accidents.showAllRule());
+        model.addAttribute("accident", accidents.findAccidentByID(id));
+        model.addAttribute("types", accidents.getAllAccidentType());
+        model.addAttribute("rules", accidents.getAllRules());
         return "accident/edit";
     }
 
--- src/main/java/ru/job4j/car/accident/controller/IndexControl.java
+++ src/main/java/ru/job4j/car/accident/controller/IndexControl.java
@@ -3,16 +3,19 @@ package ru.job4j.car.accident.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.car.accident.repository.AccidentMem;
-
+import ru.job4j.car.accident.repository.AccidentJdbcTemplate;
 
 @Controller
 public class IndexControl {
-//    private final AccidentMem rsl = AccidentMem.of();
+//    private final AccidentJdbcTemplate accidents;
+//
+//    public IndexControl(AccidentJdbcTemplate accidents) {
+//        this.accidents = accidents;
+//    }
 //
 //    @GetMapping("/")
 //    public String index(Model model) {
-//        model.addAttribute("allAccident", rsl.showAllAccident());
+//        model.addAttribute("allAccident", accidents.getAllAccidents());
 //        return "index";
 //    }
 }
